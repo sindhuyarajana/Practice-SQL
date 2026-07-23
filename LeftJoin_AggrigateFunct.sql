790e91cb69b558336d3a49f059ec0839fd3c58b4
sindhuyarajana <samsindhuy123@gmail.com>
2026-07-23 16:07:49 +0530

    Left Join using Aggrigate MAX Function

--- LeftJoin_AggrigateFunct.sql
+++ LeftJoin_AggrigateFunct.sql
@@ -11,3 +11,10 @@ FROM employee
 LEFT JOIN department
 ON employee.department_id = department.Department_id
 GROUP BY department.Department_name;
+
+SELECT department.Department_name,
+       MAX(employee.salary) AS Highest_Salary
+FROM employee
+LEFT JOIN department
+ON employee.Department_id = department.Department_id
+GROUP BY department.Department_name;
