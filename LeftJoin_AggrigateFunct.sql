d1a0329030a548f7f42b09546bf97f54cfad0a0f
sindhuyarajana <samsindhuy123@gmail.com>
2026-07-23 15:59:22 +0530

    left join using aggrigate AVG funct

--- LeftJoin_AggrigateFunct.sql
+++ LeftJoin_AggrigateFunct.sql
@@ -4,3 +4,10 @@ FROM employee
 LEFT JOIN department
 ON employee.Department_id = department.Department_id
 GROUP BY department.Department_name;
+
+SELECT department.department_name,
+       AVG(employee.salary) AS AVG_Salary
+FROM employee
+LEFT JOIN department
+ON employee.department_id = department.Department_id
+GROUP BY department.Department_name;
