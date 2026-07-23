0f9ae301d40d9fda0c07c863454727a3ca913119
sindhuyarajana <samsindhuy123@gmail.com>
2026-07-23 15:21:57 +0530

    Another Query using ORDER BY

--- LeftJOIN_ORDERBY.sql
+++ LeftJOIN_ORDERBY.sql
@@ -6,3 +6,11 @@ LEFT JOIN department
 ON employee.Department_id = department.Department_id
 ORDER BY department.Department_name ASC,
 		 employee.salary DESC;
+         
+SELECT employee.name,
+       employee.country,
+       department.department_name
+FROM employee
+LEFT JOIN department
+ON employee.department_id = department.department_id
+ORDER BY employee.country ASC;
