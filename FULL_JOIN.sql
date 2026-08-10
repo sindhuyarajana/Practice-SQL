4b1a3c6bf7fbda05c580d143c2c1fc861ab22a2e
sindhuyarajana <samsindhuy123@gmail.com>
2026-08-10 12:34:44 +0530

    displaying all books and authors using full join

--- FULL_JOIN.sql
+++ FULL_JOIN.sql
@@ -37,3 +37,17 @@ ON book.author_id = author.author_id
 WHERE author.author_name IS NOT NULL ;
 
 describe book;
+
+SELECT b.BOOK_NAME,
+       a.AUTHOR_NAME
+FROM Book b
+LEFT JOIN Author a
+    ON b.AUTHOR_ID = a.AUTHOR_ID
+
+UNION
+
+SELECT b.BOOK_NAME,
+       a.AUTHOR_NAME
+FROM Book b
+RIGHT JOIN Author a
+    ON b.AUTHOR_ID = a.AUTHOR_ID;
